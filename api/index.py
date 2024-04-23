3c6b9f5021ddfc4fa8447841fb4a6927006e66bd
sanamjummani <sanamajeed58@gmail.com>
2024-04-23 20:43:10 +0300

    post request created

--- api/index.py
+++ api/index.py
@@ -1,4 +1,7 @@
 from http.server import BaseHTTPRequestHandler
+import cgi
+import ast
+from extractor.extractor import Extract
  
 class handler(BaseHTTPRequestHandler):
  
@@ -6,5 +9,35 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        self.wfile.write('Hello'.encode('utf-8'))
         return
+    
+    def do_POST(self):
+        
+        form = cgi.FieldStorage(
+            fp=self.rfile, 
+            headers=self.headers,
+            environ={'REQUEST_METHOD': 'POST',
+                     'CONTENT_TYPE': self.headers['Content-Type'],
+                     })
+        try:
+            if "data" in form:
+                # Assuming "data" contains multiple items
+                data = ast.literal_eval(form.getvalue('data'))
+                extract = Extract(data)
+                final_message = str(extract.send_mail_flag())
+                self.send_response(200)
+                self.send_header('Content-type','text/plain')
+                self.end_headers()
+                self.wfile.write(final_message.encode('utf-8'))
+            else:
+                self.send_response(400)
+                self.send_header('Content-type','text/plain')
+                self.end_headers()
+                self.wfile.write(b"No data field found.")
+        except Exception as e:
+            self.send_response(400)
+            self.send_header('Content-type','text/plain')
+            self.end_headers()
+            self.wfile.write(b"Invalid Data.")
+        return 
